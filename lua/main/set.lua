35ce413e6a45f6678536400379d7f639a0d2efab
Or Cohen <orc2004@gmail.com>
2024-09-23 12:52:13 +0300

    changed to wrap with eol symbol

--- lua/main/set.lua
+++ lua/main/set.lua
@@ -4,22 +4,20 @@ vim.g.maplocalleader = " "
 vim.opt.showmode = false
 
 vim.opt.number = true
-
 vim.opt.relativenumber = true
 
 vim.opt.cursorline = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = ">>", trail = "·", nbsp = "+" }
+vim.opt.listchars = { tab = ">>", trail = "·", nbsp = "+", eol = "↵" }
 
 vim.opt.hlsearch = true
 vim.opt.inccommand = "split"
 
 vim.opt.signcolumn = "yes"
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.breakindent = true
-
 
 vim.opt.scrolloff = 8
 
