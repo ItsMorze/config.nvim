215ca2fc230b2461bf89a1f3a0e97a7a9d046727
Or Cohen <orc2004@gmail.com>
2024-09-25 11:08:45 +0300

    added some config to oil (made it float!)

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,12 +4,28 @@ return {
 
     config = function()
         require("oil").setup({
+            defualt_file_explorer = true,
+            delete_to_trash = true,
+            skip_confirm_for_simple_edits = true,
+
+            float = {
+                padding = 2,
+                max_width = 90,
+                max_height = 0,
+            },
+            win_options = {
+                wrap = true,
+                winblend = 0,
+            },
+
             keymaps = {
                 ["<C-h>"] = false,
                 ["<C-l>"] = false,
+                ["<C-c>"] = false,
+                ["q"] = "actions.close",
             },
         })
 
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+        vim.keymap.set("n", "-", "<CMD>Oil --float<CR>")
     end,
 }
