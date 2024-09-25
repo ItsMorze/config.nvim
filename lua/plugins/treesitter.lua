a220dc8b3e3cecfd99f96a2c61dee72de995ee2c
Or Cohen <orc2004@gmail.com>
2024-09-25 11:06:49 +0300

    used events to make lazy more efficient

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,25 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPre",
     build = ":TSUpdate",
 
     config = function()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "cpp", "lua", "json", "vim", "vimdoc", "bash", "java", "python", "rust" },
+            ensure_installed = {
+                "c",
+                "cpp",
+                "lua",
+                "json",
+                "vim",
+                "vimdoc",
+                "bash",
+                "java",
+                "python",
+                "rust",
+                "markdown",
+            },
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
