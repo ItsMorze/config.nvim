82840f62be91db5f361f1b1284f8f58ccc7d385f
Or Cohen <orc2004@gmail.com>
2024-10-07 15:33:57 +0300

    changed colors and added jumping to todo-comments highlights

--- lua/plugins/todo-highlight.lua
+++ lua/plugins/todo-highlight.lua
@@ -4,6 +4,23 @@ return {
     event = "VimEnter",
 
     config = function()
-        require("todo-comments").setup({})
+        local todo_comments = require("todo-comments")
+
+        todo_comments.setup({
+            -- BUG: error
+            -- HACK: warning
+            -- WARNING: warning
+            -- TODO: info
+            -- NOTE: hint
+            -- TESTING: test
+            -- PERFORMANCE: default
+            colors = {
+                default = { "Normal" },
+                test = { "Tag" },
+            },
+        })
+
+        vim.keymap.set("n", "]t", todo_comments.jump_next, { desc = "Next todo comment" })
+        vim.keymap.set("n", "[t", todo_comments.jump_prev, { desc = "Previous todo comment" })
     end,
 }
