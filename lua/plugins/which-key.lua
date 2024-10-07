07e1a12fa7bfa6efe06b2e067c122e1fe2885570
Or Cohen <orc2004@gmail.com>
2024-10-07 15:37:46 +0300

    changed from name to group in which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,13 +5,12 @@ return {
         require("which-key").setup()
 
         require("which-key").add({
-            { "<leader>c", name = "[C]ode", mode = { "n", "x" } },
-            { "<leader>d", name = "[D]ocument" },
-            { "<leader>r", name = "[R]ename" },
-            { "<leader>s", name = "[S]earch" },
-            { "<leader>w", name = "[W]orkspace" },
+            { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+            { "<leader>d", group = "[D]ocument" },
+            { "<leader>s", group = "[S]earch" },
+            { "<leader>w", group = "[W]orkspace" },
             { "<leader>t", group = "[T]oggle" },
-            { "<leader>h", name = "[H]arpoon" },
+            { "<leader>h", group = "[H]arpoon" },
         })
     end,
 }
