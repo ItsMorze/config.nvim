9db9ae9ab1baea08c321a4ca950190840ea170fa
Or Cohen <orc2004@gmail.com>
2024-10-29 02:18:33 +0200

    set lazy install colorscheme and disabled change detection

--- lua/main/lazy.lua
+++ lua/main/lazy.lua
@@ -14,4 +14,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    install = {
+        colorscheme = { "onedark" },
+    },
+    change_detection = {
+        notify = false,
+    },
+})
