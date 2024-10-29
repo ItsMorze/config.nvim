4fbeecc2632646c05c7c519e8956cdcda14af751
Or Cohen <orc2004@gmail.com>
2024-10-29 03:20:42 +0200

    changed some highlight groups in the colorscheme - NonText now links to WinSeparator - LspInlayHint now links to FloatBorder - now using the right highlight group for disabling italic for comments   (@comment & @lsp.type.comment instead of Comment)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,8 +13,12 @@ return {
 
         onedark.load()
 
-        vim.cmd.hi("Comment gui=none")
-
         require("colorizer").setup()
+        vim.api.nvim_set_hl(0, "NonText", { link = "WinSeparator" })
+        vim.api.nvim_set_hl(0, "LspInlayHint", { link = "FloatBorder" })
+
+        -- disable italic for comments
+        vim.cmd.hi("@comment gui=none")
+        vim.cmd.hi("@lsp.type.comment gui=none")
     end,
 }
