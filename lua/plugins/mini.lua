101e9b5fa9301a3b42fca92d72de3e3ef786d226
Or Cohen <orc2004@gmail.com>
2024-10-29 03:01:37 +0200

    now using individual mini plugins instead of using just mini.nvim - added indentscope - added splitjoin - added surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,8 +1,40 @@
 return {
-    "echasnovski/mini.nvim",
+    {
+        "echasnovski/mini.ai",
+        event = "VeryLazy",
 
-    config = function()
-        require("mini.ai").setup({ n_lines = 500 })
-        -- require("mini.surround").setup() -- doesn't work properly for some reason
-    end,
+        opts = {
+            n_lines = 500,
+        },
+    },
+    {
+        "echasnovski/mini.surround",
+        event = "VeryLazy",
+
+        opts = {
+            n_lines = 200,
+        },
+    },
+    {
+        "echasnovski/mini.splitjoin",
+        event = "VeryLazy",
+
+        opts = {
+            mappings = { toggle = "gs" },
+        },
+    },
+    {
+        "echasnovski/mini.indentscope",
+        event = "VeryLazy",
+
+        opts = {
+            draw = {
+                animation = function() return 0 end,
+            },
+            symbol = "│",
+            options = {
+                try_as_border = true,
+            },
+        },
+    },
 }
